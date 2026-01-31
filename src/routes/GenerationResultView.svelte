b45c8649522124cea8c0f527b759fe34191e49f2
Reimnop <rayngu2005@gmail.com>
2026-01-31 20:52:49 +0700

    Get rid of append

--- src/routes/GenerationResultView.svelte
+++ src/routes/GenerationResultView.svelte
@@ -75,9 +75,7 @@
       const a = document.createElement("a");
       a.href = url;
       a.download = filename;
-      document.body.appendChild(a);
       a.click();
-      document.body.removeChild(a);
     } finally {
       URL.revokeObjectURL(url);
     }
